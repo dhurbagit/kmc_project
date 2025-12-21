491168eb55de686f091214914279188fcb48b4c7
Dhurba dhakal <dhurba179@gmail.com>
2025-12-21 15:38:50 +0545

    updating index of indicator

--- resources/views/core/indicator/index.blade.php
+++ resources/views/core/indicator/index.blade.php
@@ -12,13 +12,22 @@
                             <div class="col-lg-12">
                                 <div class="form-group">
                                     <label for="name_en">Source Type</label>
-                                    <select name="source_type" class="form-control" >
+                                    <select name="source_type" class="form-control">
                                         <option value="">Select Source</option>
-                                        <option value="SDG">SDG</option>
-                                        <option value="ISO37120">ISO 37120</option>
-                                        <option value="ISO37122">ISO 37122</option>
-                                        <option value="SCI">SCI</option>
+                                        <option value="SDG" {{ old('source_type') == 'SDG' ? 'selected' : '' }}>
+                                            SDG
+                                        </option>
+                                        <option value="ISO37120" {{ old('source_type') == 'ISO37120' ? 'selected' : '' }}>
+                                            ISO 37120
+                                        </option>
+                                        <option value="ISO37122" {{ old('source_type') == 'ISO37122' ? 'selected' : '' }}>
+                                            ISO 37122
+                                        </option>
+                                        <option value="SCI" {{ old('source_type') == 'SCI' ? 'selected' : '' }}>
+                                            SCI
+                                        </option>
                                     </select>
+
                                     @error('source_type')
                                         <span class="text-danger">{{ $message }}</span>
                                     @enderror
@@ -27,9 +36,9 @@
                             <div class="col-lg-12">
                                 <div class="form-group">
                                     <label class="form-label">Indicator Code</label>
-                                    <input type="text" name="code" class="form-control"
-                                        placeholder="e.g. 11.5.1 / 6.3 / DRR-01" >
-                                        @error('code')
+                                    <input type="text" name="code" class="form-control" value="{{ old('code') }}"
+                                        placeholder="e.g. 11.5.1 / 6.3 / DRR-01">
+                                    @error('code')
                                         <span class="text-danger">{{ $message }}</span>
                                     @enderror
                                 </div>
@@ -38,8 +47,8 @@
                                 <div class="form-group">
                                     <label class="form-label">Short Name</label>
                                     <input type="text" name="short_name" class="form-control"
-                                        placeholder="e.g. Disaster mortality rate" >
-                                        @error('short_name')
+                                        value="{{ old('short_name') }}" placeholder="e.g. Disaster mortality rate">
+                                    @error('short_name')
                                         <span class="text-danger">{{ $message }}</span>
                                     @enderror
                                 </div>
@@ -47,13 +56,15 @@
                             <div class="col-lg-12">
                                 <div class="form-group">
                                     <label class="form-label">Goal Code</label>
-                                    <input type="text" name="goal_code" class="form-control" placeholder="e.g. 11">
+                                    <input type="text" name="goal_code" class="form-control" placeholder="e.g. 11"
+                                        value="{{ old('goal_code') }}">
                                 </div>
                             </div>
                             <div class="col-lg-12">
                                 <div class="form-group">
                                     <label class="form-label">Target Code</label>
-                                    <input type="text" name="target_code" class="form-control" placeholder="e.g. 11.5">
+                                    <input type="text" name="target_code" class="form-control" placeholder="e.g. 11.5"
+                                        value="{{ old('target_code') }}">
                                 </div>
                             </div>
                             <div class="col-lg-12">
@@ -61,12 +72,29 @@
                                     <label class="form-label">Unit</label>
                                     <select name="unit" class="form-control">
                                         <option value="">Select Unit</option>
-                                        <option value="%">%</option>
-                                        <option value="minutes">Minutes</option>
-                                        <option value="score">Score</option>
-                                        <option value="ratio">Ratio</option>
-                                        <option value="per 100,000 population">Per 100,000 Population</option>
+
+                                        <option value="%" {{ old('unit') == '%' ? 'selected' : '' }}>
+                                            %
+                                        </option>
+
+                                        <option value="minutes" {{ old('unit') == 'minutes' ? 'selected' : '' }}>
+                                            Minutes
+                                        </option>
+
+                                        <option value="score" {{ old('unit') == 'score' ? 'selected' : '' }}>
+                                            Score
+                                        </option>
+
+                                        <option value="ratio" {{ old('unit') == 'ratio' ? 'selected' : '' }}>
+                                            Ratio
+                                        </option>
+
+                                        <option value="per 100,000 population"
+                                            {{ old('unit') == 'per 100,000 population' ? 'selected' : '' }}>
+                                            Per 100,000 Population
+                                        </option>
                                     </select>
+
                                 </div>
                             </div>
                             <div class="col-lg-12">
@@ -81,7 +109,7 @@
                             <div class="col-lg-12">
                                 <div class="form-group">
                                     <label class="form-label">Description</label>
-                                    <textarea name="description" class="form-control" rows="3" placeholder="Full definition of indicator"></textarea>
+                                    <textarea name="description" class="form-control" rows="3" placeholder="Full definition of indicator">{{ old('description') }}</textarea>
                                 </div>
                             </div>
                             <div class="col-lg-12">
